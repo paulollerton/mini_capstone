1b98394f57a6ce5c4316d03deb839992ab288a9f
paulollerton <paul.ollerton@gmail.com>
2019-05-08 11:12:11 -0700

    adds product seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-product = Product.new(name: "T-800", price: 500 , image_url: "https://www.sideshow.com/storage/product-images/903469/t-800-endoskeleton-the-terminator_terminator_gallery_5c4cad070903e.jpg", description: "Helpful Robot Buddy")
+product = Product.create(name: "T-800", price: 500 , image_url: "https://www.sideshow.com/storage/product-images/903469/t-800-endoskeleton-the-terminator_terminator_gallery_5c4cad070903e.jpg", description: "Helpful Robot Buddy")
 
-product = Product.new(name: "Llama", price: 2000 , image_url: "https://www.mercurynews.com/wp-content/uploads/2019/02/05075011.jpg", description: "Sheep Protector, Competitive Spitter")
+product = Product.create(name: "Llama", price: 2000 , image_url: "https://www.mercurynews.com/wp-content/uploads/2019/02/05075011.jpg", description: "Sheep Protector, Competitive Spitter")
 
-product = Product.new(name: "Mason Jar", price: 5 , image_url: "https://images-na.ssl-images-amazon.com/images/I/41e09bhmgaL._SX425_.jpg", description: "Good at storing jam.")
+product = Product.create(name: "Mason Jar", price: 5 , image_url: "https://images-na.ssl-images-amazon.com/images/I/41e09bhmgaL._SX425_.jpg", description: "Good at storing jam.")
